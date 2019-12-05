23739af12c3674082b5d8f75c8a746c878594f25
dword <sebastianlandw@gmail.com>
2019-12-05 00:02:20 +0000

    upgrade base and generate files

--- test/eslint.js
+++ test/eslint.js
@@ -1,9 +1,7 @@
 import { CLIEngine } from 'eslint'
-import importFresh from 'import-fresh'
 
 export default (code, filename) => {
-  const config = importFresh('@dword-design/eslint-config')
-  const eslint = new CLIEngine({ baseConfig: config })
+  const eslint = new CLIEngine()
   const result = eslint.executeOnText(code, filename)
   return result.errorCount === 0
 }
