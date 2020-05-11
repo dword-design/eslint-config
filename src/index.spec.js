5a57611eb551896a9ddf9fd9535b17a88c90d476
foo <foo@bar.de>
2020-05-11 21:36:06 +0200

    chore: refactor tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,9 @@
-import { endent } from '@dword-design/functions'
+import { endent, mapValues } from '@dword-design/functions'
 import execa from 'execa'
 import withLocalTmpDir from 'with-local-tmp-dir'
 import outputFiles from 'output-files'
 
-const eslint = (files, { nodeEnv } = {}) =>
+const runTest = ({ files, match }) => () =>
   withLocalTmpDir(async () => {
     await outputFiles({
       ...files,
@@ -16,85 +16,80 @@ const eslint = (files, { nodeEnv } = {}) =>
       ),
     })
     try {
-      await execa('eslint', ['--ext', '.js,.json', '.'], {
+      const { all } = await execa('eslint', ['--ext', '.js,.json', '.'], {
         all: true,
-        env: { ...process.env, NODE_ENV: nodeEnv },
       })
-      return ''
-    } catch ({ all }) {
-      return all
+      expect(all).toBeFalsy()
+    } catch (error) {
+      expect(error.all).toMatch(match)
     }
   })
 
 export default {
-  'dev dependency in root': async () =>
-    expect(
-      await eslint({
-        'node_modules/foo/index.js': 'export default 1',
-        'package.json': JSON.stringify(
-          {
-            devDependencies: {
-              foo: '^1.0.0',
-            },
+  'dev dependency in root': {
+    files: {
+      'node_modules/foo/index.js': 'export default 1',
+      'package.json': JSON.stringify(
+        {
+          devDependencies: {
+            foo: '^1.0.0',
           },
-          undefined,
-          2
-        ),
-        'index.js': endent`
+        },
+        undefined,
+        2
+      ),
+      'index.js': endent`
         import foo from 'foo'
 
         console.log(foo)
       `,
-      })
-    ).toMatch(
-      "error  'foo' should be listed in the project's dependencies, not devDependencies"
-    ),
-  'dev dependency in source': async () =>
-    expect(
-      await eslint({
-        'node_modules/foo/index.js': 'export default 1',
-        'package.json': endent`
+    },
+    match:
+      "'foo' should be listed in the project's dependencies, not devDependencies",
+  },
+  'dev dependency in source': {
+    files: {
+      'node_modules/foo/index.js': 'export default 1',
+      'package.json': endent`
         {
           "devDependencies": {
             "foo": "^1.0.0"
           }
         }
       `,
-        'src/index.js': endent`
+      'src/index.js': endent`
         import foo from 'foo'
 
         console.log(foo)
       `,
-      })
-    ).toMatch(
-      "error  'foo' should be listed in the project's dependencies, not devDependencies"
-    ),
-  'indent: valid': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+    match:
+      "'foo' should be listed in the project's dependencies, not devDependencies",
+  },
+  'indent: valid': {
+    files: {
+      'test.js': endent`
         export default () => {
           console.log('foo')
         }
 
       `,
-      })
-    ).toEqual(''),
-  'indent: invalid': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'indent: invalid': {
+    files: {
+      'test.js': endent`
         export default () => {
             console.log('foo')
         }
 
       `,
-      })
-    ).toMatch('error  Delete `··`  prettier/prettier'),
-  'json: valid': async () =>
-    expect(
-      await eslint({
-        'test.json': endent`
+    },
+    match: 'Delete `··`  prettier/prettier',
+  },
+  'json: valid': {
+    files: {
+      'test.json': endent`
         {
           "foo": "bar",
           "bar": {
@@ -105,139 +100,132 @@ export default {
           }
         }
       `,
-      })
-    ).toEqual(''),
-  'json: syntax error': async () =>
-    expect(
-      await eslint({
-        'test.json': endent`
+    },
+  },
+  'json: syntax error': {
+    files: {
+      'test.json': endent`
         {
           "foo":
         }
       `,
-      })
-    ).toMatch('error  Unexpected token'),
-  'json: no indent': async () =>
-    expect(
-      await eslint({
-        'test.json': endent`
+    },
+    match: 'Unexpected token',
+  },
+  'json: no indent': {
+    files: {
+      'test.json': endent`
         {
         "foo": "bar"
         }
       `,
-      })
-    ).toMatch('error  Format Error: expected "  "'),
-  'json: indent too big': async () =>
-    expect(
-      await eslint({
-        'test.json': endent`
-        {
-            "foo": "bar"
-        }
-      `,
-      })
-    ).toMatch('error  Format Error: unexpected "  "'),
-  'jsx: with var': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
-      const Foo = <div>Hello world</div>
-      export default (
-        <div>
-          <Foo />
-        </div>
-      )
-
+    },
+    match: 'Format Error: expected "  "',
+  },
+  'json: indent too big': {
+    files: {
+      'test.json': endent`
+      {
+          "foo": "bar"
+      }
     `,
-      })
-    ).toEqual(''),
-  'jsx: valid': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+    match: 'Format Error: unexpected "  "',
+  },
+  'jsx: with var': {
+    files: {
+      'test.js': endent`
+        const Foo = <div>Hello world</div>
+        export default (
+          <div>
+            <Foo />
+          </div>
+        )
+
+      `,
+    },
+  },
+  'jsx: valid': {
+    files: {
+      'test.js': endent`
         export default <div>Hello world</div>
         
       `,
-      })
-    ).toEqual(''),
-  'package.json: valid': async () =>
-    expect(
-      await eslint({
-        'package.json': endent`
+    },
+  },
+  'package.json: valid': {
+    files: {
+      'package.json': endent`
         {
           "name": "foo",
           "version": "1.0.0"
         }
       `,
-      })
-    ).toEqual(''),
-  'package.json: unsorted': async () =>
-    expect(
-      await eslint({
-        'package.json': endent`
-        {
-          "version": "1.0.0",
-          "name": "foo"
-        }
-      `,
-      })
-    ).toMatch('error  JSON is not sorted'),
-  'prefer-arrow': async () => {
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'package.json: unsorted': {
+    files: {
+      'package.json': endent`
+      {
+        "version": "1.0.0",
+        "name": "foo"
+      }
+    `,
+    },
+    match: 'JSON is not sorted',
+  },
+  'arrow function': {
+    files: {
+      'test.js': endent`
         export default () => console.log('foo')
 
       `,
-      })
-    ).toEqual('')
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'function block': {
+    files: {
+      'test.js': endent`
         export default function () { console.log('foo') }
 
       `,
-      })
-    ).toMatch('error  Prefer using arrow functions over plain functions')
+    },
+    match: 'Prefer using arrow functions over plain functions',
   },
-  'prod dependency in src': async () =>
-    expect(
-      await eslint({
-        'node_modules/foo/index.js': endent`
+  'prod dependency in src': {
+    files: {
+      'node_modules/foo/index.js': endent`
         export default 1
         
       `,
-        'package.json': JSON.stringify(
-          {
-            dependencies: {
-              foo: '^1.0.0',
-            },
+      'package.json': JSON.stringify(
+        {
+          dependencies: {
+            foo: '^1.0.0',
           },
-          undefined,
-          2
-        ),
-        'src/index.js': endent`
+        },
+        undefined,
+        2
+      ),
+      'src/index.js': endent`
         import foo from 'foo'
 
         console.log(foo)
 
       `,
-      })
-    ).toEqual(''),
-  'regex-spaces': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'regex-spaces': {
+    files: {
+      'test.js': endent`
         export default /  /
 
       `,
-      })
-    ).toEqual(''),
-  'restricted import: inside': async () =>
-    expect(
-      await eslint({
-        'node_modules/puppeteer/index.js': '',
-        'package.json': endent`
+    },
+  },
+  'restricted import: inside': {
+    files: {
+      'node_modules/puppeteer/index.js': '',
+      'package.json': endent`
         {
           "name": "@dword-design/puppeteer",
           "dependencies": {
@@ -245,82 +233,77 @@ export default {
           }
         }
       `,
-        'test.js': endent`
+      'test.js': endent`
         import puppeteer from 'puppeteer'
 
         console.log(puppeteer)
 
       `,
-      })
-    ).toEqual(''),
-  'restricted import: outside': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'restricted import: outside': {
+    files: {
+      'test.js': endent`
         import puppeteer from 'puppeteer'
         console.log(puppeteer)
 
       `,
-      })
-    ).toMatch(
-      "'puppeteer' import is restricted from being used. Please use '@dword-design/puppeteer' instead"
-    ),
-  semicolon: async () => {
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+    match:
+      "'puppeteer' import is restricted from being used. Please use '@dword-design/puppeteer' instead",
+  },
+  valid: {
+    files: {
+      'test.js': endent`
         console.log()
       
       `,
-      })
-    ).toEqual('')
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  semicolon: {
+    files: {
+      'test.js': endent`
         console.log();
 
       `,
-      })
-    ).toMatch('error  Delete `;`  prettier/prettier')
+    },
+    match: 'error  Delete `;`',
   },
-  'test: dev dependency': async () =>
-    expect(
-      await eslint({
-        'node_modules/foo/index.js': endent`
+  'test: dev dependency': {
+    files: {
+      'node_modules/foo/index.js': endent`
         export default 1
 
       `,
-        'package.json': endent`
+      'package.json': endent`
         {
           "devDependencies": {
             "foo": "^1.0.0"
           }
         }
       `,
-        'src/index.spec.js': endent`
+      'src/index.spec.js': endent`
         import foo from 'foo'
 
         console.log(foo)
         
       `,
-      })
-    ).toEqual(''),
-  'test: restricted import': async () =>
-    expect(
-      await eslint({
-        'src/index.spec.js': endent`
+    },
+  },
+  'test: restricted import': {
+    files: {
+      'src/index.spec.js': endent`
         import puppeteer from 'puppeteer'
         console.log(puppeteer)
 
       `,
-      })
-    ).toMatch(
-      "'puppeteer' import is restricted from being used. Please use '@dword-design/puppeteer' instead"
-    ),
-  'test: imported expect': async () =>
-    expect(
-      await eslint({
-        'package.json': endent`
+    },
+    match:
+      "'puppeteer' import is restricted from being used. Please use '@dword-design/puppeteer' instead",
+  },
+  'test: imported expect': {
+    files: {
+      'package.json': endent`
         {
           "devDependencies": {
             "expect": "^1.0.0"
@@ -328,102 +311,96 @@ export default {
         }
 
       `,
-        'src/index.spec.js': endent`
+      'src/index.spec.js': endent`
         import expect from 'expect'
 
         expect(1).toEqual(1)
 
       `,
-      })
-    ).toMatch(
-      "'expect' import is restricted from being used. Please use the global 'expect' variable instead"
-    ),
-  'test: global expect': async () =>
-    expect(
-      await eslint({
-        'src/index.spec.js': endent`
-      expect(1).toEqual(1)
-      
-    `,
-      })
-    ).toEqual(''),
-  'test: prod dependency': async () =>
-    expect(
-      await eslint({
-        'node_modules/foo/index.js': endent`
+    },
+    match:
+      "'expect' import is restricted from being used. Please use the global 'expect' variable instead",
+  },
+  'test: global expect': {
+    files: {
+      'src/index.spec.js': endent`
+        expect(1).toEqual(1)
+        
+      `,
+    },
+  },
+  'test: prod dependency': {
+    files: {
+      'node_modules/foo/index.js': endent`
         export default 1
         
       `,
-        'package.json': JSON.stringify(
-          {
-            dependencies: {
-              foo: '^1.0.0',
-            },
+      'package.json': JSON.stringify(
+        {
+          dependencies: {
+            foo: '^1.0.0',
           },
-          undefined,
-          2
-        ),
-        'src/index.spec.js': endent`
+        },
+        undefined,
+        2
+      ),
+      'src/index.spec.js': endent`
         import foo from 'foo'
 
         console.log(foo)
 
       `,
-      })
-    ).toEqual(''),
-  'quotes: nested': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'quotes: nested': {
+    files: {
+      'test.js': endent`
         export default "foo 'bar'"
 
       `,
-      })
-    ).toEqual(''),
-  'quotes: unnecessary escapes': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'quotes: unnecessary escapes': {
+    files: {
+      'test.js': endent`
         export default 'foo \\'bar\\''
 
       `,
-      })
-    ).toMatch("Replace `'foo·\\'bar\\''` with `\"foo·'bar'\"`"),
-  'arrow function block': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+    match: "Replace `'foo·\\'bar\\''` with `\"foo·'bar'\"`",
+  },
+  'arrow function block': {
+    files: {
+      'test.js': endent`
         export default foo => {
           console.log(foo)
         }
 
       `,
-      })
-    ).toEqual(''),
-  'pipeline operator': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'pipeline operator': {
+    files: {
+      'test.js': endent`
         export default async () => 1 |> (x => x + 1) |> await
         
       `,
-      })
-    ).toEqual(''),
-  'deep nesting': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
+    },
+  },
+  'deep nesting': {
+    files: {
+      'test.js': endent`
         export default async () => console.log(() => (1 + 2 + 3 + 4) * 3 + 5)
       `,
-      })
-    ).toMatch('error  Insert `⏎`  prettier/prettier'),
-  'single export': async () =>
-    expect(
-      await eslint({
-        'test.js': endent`
-          export const foo = 'bar'
+    },
+    match: 'error  Insert `⏎`',
+  },
+  'single export': {
+    files: {
+      'test.js': endent`
+        export const foo = 'bar'
 
-        `,
-      })
-    ).toEqual(''),
-}
+      `,
+    },
+  },
+} |> mapValues(runTest)
